58a9467d3979b461234b85fff2d85894cbef6539
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-04-28 13:55:48 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -426,7 +426,7 @@ const Dashboard = () => {
                   <CallIcon
                     style={{
                       fontSize: 100,
-                      color: "#0b708c",
+                      color: "#0050FF",
                     }}
                   />
                 </Grid>
