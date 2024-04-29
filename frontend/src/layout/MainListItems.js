c4b11ddd3c1c3f3c6d0662c37349078e5315e29b
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-04-28 22:12:03 -0300

    Update MainListItems.js

--- frontend/src/layout/MainListItems.js
+++ frontend/src/layout/MainListItems.js
@@ -302,16 +302,7 @@ const MainListItems = (props) => {
         primary={i18n.t("mainDrawer.listItems.tickets")}
         icon={<WhatsAppIcon />}
       />
-	  
-	{showIntegrations && (  
-	  <ListItemLink
-        to="/kanban"
-        primary={i18n.t("Kanban")}
-        icon={<TableChartIcon />}
-      />
-	  )}
-
-
+      	  
       <ListItemLink
         to="/quick-messages"
         primary={i18n.t("mainDrawer.listItems.quickMessages")}
