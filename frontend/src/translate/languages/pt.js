c3ea05e8a8f83a06520e8bc203ee53dd44792893
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-04-30 18:11:52 -0300

    Update pt.js

--- frontend/src/translate/languages/pt.js
+++ frontend/src/translate/languages/pt.js
@@ -441,7 +441,7 @@ const messages = {
           tickets: "Atendimentos",
           quickMessages: "Respostas Rápidas",
           contacts: "Contatos",
-          queues: "Filas & Chatbot",
+          queues: "Departamentos & Chatbot",
           tags: "Tags",
           administration: "Administração",
           users: "Usuários",
