9fb663b27ab8908ffb06c8b29c80b8b0c7f298a6
Luis Filipe Gomes <eu.gdcarvalho@icloud.com>
2024-04-30 12:33:23 -0300

    Update index.js

--- frontend/src/pages/Kanban/index.js
+++ frontend/src/pages/Kanban/index.js
@@ -118,22 +118,6 @@ const Kanban = () => {
         cards: filteredTickets.map(ticket => ({
           id: ticket.id.toString(),
           label: "Ticket nº " + ticket.id.toString(),
-          description: (
-              <div>
-                <p>
-                  {ticket.contact.number}
-                  <br />
-                  {ticket.lastMessage}
-                </p>
-                <button 
-                  className={classes.button} 
-                  onClick={() => {
-                    handleCardClick(ticket.uuid)
-                  }}>
-                    Ver Ticket
-                </button>
-              </div>
-            ),
           title: ticket.contact.name,
           draggable: true,
           href: "/tickets/" + ticket.uuid,
